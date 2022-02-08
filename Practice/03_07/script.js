8779bc3fddaf81b3409185798828f492c7343ec2
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-02-08 07:26:28 -0500

    Making changes to excercise files

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,18 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const computer = {
+  brand: "Dell",
+  color: "gray",
+  monitorSize: 15,
+  hardDrive: {
+    brand: "Western Digital",
+    size: 500,
+    changeSize: function (newSize) {
+      this.size = newSize;
+      console.log("New Size", this.size);
+    },
+  },
+};
+
+console.log(computer.hardDrive.brand);
